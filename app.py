806fabf5cd33895bb05e0828aca9eeaed359b96f
Suliot <suliot@msn.com>
2024-02-15 09:01:30 -0700

    beta 0.3

--- app.py
+++ app.py
@@ -8,34 +8,38 @@ messages = []
 openai_client = OpenAI(api_key=openai_key)
 
 def call_api(model_chat, messages, temperature): 
-        try:
-            response = openai_client.chat.completions.create(
-            model=model_chat,
-            messages=messages,
-            temperature=temperature,
-        )
-        except Exception as e:
-            print(e)
-        else:
-            add_message("assistant", response.choices[0].message.content)
-            return response.choices[0].message.content
+    try:
+        response = openai_client.chat.completions.create(
+        model=model_chat,
+        messages=messages,
+        temperature=temperature,
+    )
+    except Exception as e:
+        print(e)
+    else:
+        add_message("assistant", response.choices[0].message.content)
+        return response.choices[0].message.content
+
 
 def add_message(role, content):
-        json_message = {
-            "role": role, 
-            "content": content
-        }
-        messages.append(json_message)
+    json_message = {
+        "role": role, 
+        "content": content
+    }
+    messages.append(json_message)
+
 
 def ask_question(question):
     add_message("user", question)
     print("-" * 67)
     print("Your question is: ", question)
     print("-" * 67)
-    print(call_api("gpt-3.5-turbo", messages, 0.5))
+    res = call_api("gpt-4", messages, 0.3)
+    print(res)
+
 
 def main():
-    add_message("system", "You are an expert in python, data science, artificial intelligence and machine learning")
+    add_message("system", "You are A helpful assistant providing truthful information")
     while True:
         question = input("Enter your question (or type 'exit' to quit): ")
         if question.lower() == 'exit':
@@ -43,5 +47,6 @@ def main():
             break
         ask_question(question)
 
+    
 if __name__ == "__main__":
     main()
